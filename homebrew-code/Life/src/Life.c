e033794bf2819c4b97af17fb417e8d7d5737a6fd
Mike Debreceni <mdebreceni@gmail.com>
2023-02-11 14:27:07 -0800

    Scan only acrtive areas, show scan behavior

--- homebrew-code/Life/src/Life.c
+++ homebrew-code/Life/src/Life.c
@@ -54,7 +54,7 @@ char cols_to_scan[64];
 char rows_to_scan[48];
 
 int16_t cursor_x_to_screen(int cursor_x) {
-    int16_t offset = 0;
+    int16_t offset = 32;
     return 4 * cursor_x + offset;
 }
 
@@ -78,6 +78,8 @@ int countNeighbors(int x, int y) {
     // [   ][x,y][   ]
     // [   ][   ][   ]
     int16_t neighbors = 0;
+    vdp_sprite_set_position(sprite_handle, cursor_x_to_screen(x), cursor_y_to_screen(y));
+    // vdp_sprite_set_position(sprite_handle, cursor_x_to_screen(0), cursor_y_to_screen(0));
 
     for(int xn = x - 1; xn <= x + 1; xn++) {
         for(int yn = y - 1; yn <= y + 1; yn++) {
@@ -95,36 +97,63 @@ int countNeighbors(int x, int y) {
 }
 
 void setRowsToScan(void) {
-    for(int x=0; x<63; x++) {
-        if(cols_to_scan[x+1]) cols_to_scan[x] = 1;
+    for(int y=0; y<47; y++) {
+        if(rows_to_scan[y+1]) rows_to_scan[y] = 1;
     }
-    for(int x=63; x>0; x--) {
-        if(cols_to_scan[x-1]) cols_to_scan[x] = 1;
+    for(int y=47; y>0; y--) {
+        if(rows_to_scan[y-1]) rows_to_scan[y] = 1;
     }
-}
 
-void plotColsToScan(void) {
-    for(int x=0; x<64; x++) {
-        if(cols_to_scan[x]) vdp_plot_color(x, 0, VDP_LIGHT_GREEN); else vdp_plot_color(x, 0, VDP_DARK_BLUE);
+    int idy = 0;
+    for(int y = 0; y < 48; y++) {
+        if(rows_to_scan[y]) {
+            rows_to_scan[idy++] = y;
+        }
     }
-}
+    rows_to_scan[idy] = (char) -1;
 
+}
 
 void plotRowsToScan(void) {
     for(int y=0; y<48; y++) {
-        if(rows_to_scan[y]) vdp_plot_color(0, y, VDP_LIGHT_GREEN); else vdp_plot_color(0, y, VDP_DARK_BLUE);
+        vdp_plot_color(0, y, VDP_DARK_BLUE);
+    }
+    for(int idy=0; idy<48 && rows_to_scan[idy] != (char) -1; idy++) {
+        int y = rows_to_scan[idy];
+        vdp_plot_color(0, y, VDP_LIGHT_GREEN);
+
     }
 }
 
 void setColsToScan(void) {
-    for (int y = 0; y<47; y++) {
-        if(rows_to_scan[y+1]) rows_to_scan[y] = 1;
+    for (int x = 0; x<63; x++) {
+        if(cols_to_scan[x+1]) cols_to_scan[x] = 1;
     }
 
-    for (int y = 47; y>0; y--) {
-        if(rows_to_scan[y-1]) rows_to_scan[y] = 1;
+    for (int x = 63; x>0; x--) {
+        if(cols_to_scan[x-1]) cols_to_scan[x] = 1;
+    }
+
+    int idx = 0;
+    for(int x = 0; x < 64; x++) {
+        if(cols_to_scan[x]) {
+            cols_to_scan[idx++] = x;
+        }
+    }
+    cols_to_scan[idx] = (char) -1;
+
+}
+void plotColsToScan(void) {
+     for(int x=0; x<64; x++) {
+        vdp_plot_color(x, 0, VDP_DARK_BLUE);
+     }
+
+    for(int idx=0; idx<64 && cols_to_scan[idx] != (char) -1; idx++) {
+        int x = cols_to_scan[idx];
+        vdp_plot_color(x, 0, VDP_LIGHT_GREEN); 
     }
 }
+
 
 void initActiveRowsColsFromLifeGrid(void) {
     memset(cols_to_scan, 0, 64);
@@ -153,22 +182,22 @@ void initGrid(void) {
     lifeGrid[33][24] = true;
 
     // Glider
-    lifeGrid[10][10] = true;
-    lifeGrid[11][11] = true;
-    lifeGrid[11][12] = true;
-    lifeGrid[9][12] = true;
-    lifeGrid[10][12] = true;
+    // lifeGrid[10][10] = true;
+    // lifeGrid[11][11] = true;
+    // lifeGrid[11][12] = true;
+    // lifeGrid[9][12] = true;
+    // lifeGrid[10][12] = true;
 
     // Blinker
-    lifeGrid[13][21] = true;
-    lifeGrid[13][22] = true;
-    lifeGrid[13][23] = true;
+    // lifeGrid[13][21] = true;
+    // lifeGrid[13][22] = true;
+    // lifeGrid[13][23] = true;
     
     // Square 
-    lifeGrid[52][42] = true;
-    lifeGrid[53][42] = true;
-    lifeGrid[52][43] = true;
-    lifeGrid[53][43] = true;
+    // lifeGrid[52][42] = true;
+    // lifeGrid[53][42] = true;
+    // lifeGrid[52][43] = true;
+    // lifeGrid[53][43] = true;
 
     initActiveRowsColsFromLifeGrid();    
 }
@@ -188,8 +217,10 @@ void plotGrid(void) {
 int runGeneration(bool (*callback_func)(void)) {
     bool keepgoing = true;
     // count neighbors
-    for(int x = 0; x < 64; x++) {
-        for(int y = 0; y < 48; y++) {
+    for(int idx = 0; idx < 64 && cols_to_scan[idx] != (char) -1; idx++) {
+        int x = cols_to_scan[idx];
+        for(int idy = 0; idy < 48 && rows_to_scan[idy] != (char) -1; idy++) {
+            int y = rows_to_scan[idy];
             neighborCount[x][y] = countNeighbors(x,y);
         }
     }
@@ -199,10 +230,11 @@ int runGeneration(bool (*callback_func)(void)) {
     memset(row_was_active, 0, 48);
 
     // calculate next generation
-    for (int x = 0; x < 64; x++) {
-        if (cols_to_scan[x]) {
-            for (int y = 0; y < 48; y++) {
-                if (rows_to_scan[y]) {
+    int x, y;
+    for (int idx=0; idx < 64 && cols_to_scan[idx] != (char) -1; idx++) {
+        x = cols_to_scan[idx];
+            for (int idy = 0; idy < 48 && rows_to_scan[idy] != (char) -1; idy++) {
+                y = rows_to_scan[idy];
                     int c = neighborCount[x][y];
                     if (lifeGrid[x][y] == true) {
                         // cell is currently alive
@@ -225,9 +257,9 @@ int runGeneration(bool (*callback_func)(void)) {
                             row_was_active[y] = true;
                         }
                     }
-                }
+                //}
             }
-        }
+        //}
     }
 
     memcpy(cols_to_scan, col_was_active, 64);
@@ -293,7 +325,7 @@ bool handle_input(void) {
 
         if (cursor_y <= 0) cursor_y = 0;
         if (cursor_y > 180) cursor_y = 180;
-        vdp_sprite_set_position(sprite_handle, cursor_x, cursor_y);
+        // vdp_sprite_set_position(sprite_handle, cursor_x, cursor_y);
     } else {
         stepSize = 4;
     }
@@ -308,8 +340,8 @@ void main2() {
     for (int i = 0; i < 256; i++) {
         vdp_set_sprite_pattern(i, cursor_sprite_small);
     }
-    //sprite_handle = vdp_sprite_init(0, 0, VDP_WHITE);
-    //vdp_sprite_set_position(sprite_handle, cursor_x, cursor_y);
+    sprite_handle = vdp_sprite_init(0, 0, VDP_WHITE);
+//    vdp_sprite_set_position(sprite_handle, cursor_x, cursor_y);
 
     bool keepgoing = true;
     initGrid();
